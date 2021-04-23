adbdbda6a80fddf528dd4bfe2a411dceca8f76fa
Irina Koskina <koskinairina659@gmail.com>
2021-04-23 13:08:22 +0300

    lw8 task2

---
+++ lw8/task2/script.js
@@ -0,0 +1,38 @@
+function calc(string) {
+  if (typeof string === 'string') {
+    const sampleString = /\(*[+\-*/]\s+\d+\s+\d+\)*/;
+    let match = string.match(sampleString);
+    if (match !== null) {
+      match[0] =  match[0].replace(/\(*/, '');
+      match[0] = match[0].replace(/\)*$/, '');
+      // console.log(match);
+      let arr = match[0].split(" ");
+      let operator = arr[0];
+      let firstTerm = Number(arr[1]);
+      let secondTerm = Number(arr[2]);
+      let result = 0;
+      if (operator === '+') {
+        result = firstTerm + secondTerm;
+      }
+      if (operator === '-') {
+        result = firstTerm - secondTerm;
+      }
+      if (operator === '*') {
+        result = firstTerm * secondTerm;
+      }
+      if (operator === '/') {
+        result = firstTerm / secondTerm;
+      }
+      let newString = string.replace(sampleString, result);
+      if (newString.length === String(result).length) {
+        return result;
+      } else {
+        return calc(newString);
+      }
+    } else {
+      return 'Cовпадений не найдено';
+    }
+  } else {
+    return 'Введите строку';
+  }
+}
